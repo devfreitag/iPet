e5b041d728621340a1cd497b05737c31549e6513
Alex Freitag <alex.f@quantich.com.br>
2019-09-24 23:16:40 -0300

    inclusão de informações para ser usado no flatlist e transformado botão em componente externo para poder ser reutilizado futuramente

--- src/screens/Adocao.js
+++ src/screens/Adocao.js
@@ -11,63 +11,109 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Home extends Component {
-
-  static navigationOptions = ({ navigation, screenProps }) => ({
+  static navigationOptions = ({navigation, screenProps}) => ({
     title: null,
-    headerLeft: 
+    headerLeft: (
       <View style={{margin: 10, flex:1, flexDirection: 'row', justifyContent: 'center'}}>
-        <Icon style={{marginTop: 5}}name={'chevron-left'} size={20}  onPress={ () => { navigation.goBack() }} />
+        <Icon
+          style={{marginTop: 5}}
+          name={'arrow-left'}
+          size={20}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
         <Text style={{fontSize: 20, marginLeft: 10, /*fontWeight: 'bold'*/}}>Home</Text>
       </View>
+    )
   });
 
   state = {
     data: [
-      {id: '1', name: 'Luna', age: 2, time: 'anos', description: 'bla bla bla bla'},
-      {id: '2', name: 'Laila', age: 5, time: 'meses', description: 'bla bla bla bla'},
-      {id: '3', name: 'Rambo', age: 6, time: 'meses', description: 'bla bla bla bla'},
-    ]
+      {
+        id: '1',
+        name: 'Luna',
+        age: 2,
+        time: 'anos',
+        description: 'bla bla bla bla',
+      },
+      {
+        id: '2',
+        name: 'Laila',
+        age: 5,
+        time: 'meses',
+        description: 'bla bla bla bla',
+      },
+      {
+        id: '3',
+        name: 'Rambo',
+        age: 6,
+        time: 'meses',
+        description: 'bla bla bla bla',
+      },
+      {
+        id: '4',
+        name: 'Zuca',
+        age: 4,
+        time: 'anos',
+        description: 'bla bla bla bla',
+      },
+    ],
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={{justifyContent: 'flex-start', alignItems:'center', marginTop: 20, marginBottom: 30}}>
+        <View style={styles.viewTitle}>
           <Text style={styles.title}>Adotar</Text>
         </View>
         <FlatList
           data={this.state.data}
           keyExtractor={item => item.id}
-          renderItem={({ item }) => {
+          renderItem={({item}) => {
             return (
               <View style={styles.item}>
                 <View style={styles.viewImage}>
-                  <Text style={{color: 'black'}}>Imagem do animal aqui</Text>
+                  <Image
+                    source={require('../../imgs/dog1.jpg')}
+                    style={styles.image}
+                  />
                 </View>
-                <View style={{/*backgroundColor: 'green',*/ justifyContent: 'center', marginLeft: 10}}>
+                <View style={styles.viewData}>
                   <Text style={styles.text}>{item.name}</Text>
-                  <Text style={styles.text}>{item.age} {item.time}</Text>
-                  <Text style={{fontSize: 18}}>{item.description}</Text>
+                  <Text style={styles.text}>
+                    {item.age} {item.time}
+                  </Text>
+                  <Text style={styles.textDescription}>{item.description}</Text>
+                  <TouchableHighlight style={styles.button}>
+                    <Text style={styles.textButton}> Mais informações</Text>
+                  </TouchableHighlight>
                 </View>
               </View>
             );
           }}
         />
-    </View>
+      </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    //justifyContent: 'center',
-    //alignItems: 'center',
   },
   text: {
     color: 'black',
     fontSize: 20,
+  },
+  textDescription: {
+    fontSize: 16,
+    fontStyle: 'italic',
+  },
+  textButton: {
+    color: 'white',
+    fontSize: 18,
   },
   title: {
     color: '#2fb7a7',
@@ -75,21 +121,44 @@ const styles = StyleSheet.create({
     fontFamily: 'GROBOLD',
   },
   viewImage: {
-    height: 100,
-    width: 100,
-    borderColor: 'purple',
-    borderWidth: 3,
+    height: 120,
+    width: 120,
     justifyContent: 'center',
-    borderRadius: 20
+    borderRadius: 20,
+  },
+  viewData: {
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  viewTitle: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 30,
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
   },
   item: {
-    alignItems: "center",
+    alignItems: 'center',
     //backgroundColor: "#dcda48",
     flexGrow: 1,
     margin: 4,
     padding: 20,
     flexDirection: 'row',
     borderBottomColor: 'grey',
-    borderBottomWidth: 2
+    borderBottomWidth: 2,
+  },
+  button: {
+    backgroundColor: '#2fb7a7',
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+    width: Dimensions.get('window').width / 2,
+    height: Dimensions.get('window').height / 22,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,35 +6,30 @@ import {
   TouchableHighlight,
   Dimensions,
   Image,
-  Button,
 } from 'react-native';
+import ButtonRoutes from '../components/ButtonRoutes';
 
 export default class Home extends Component {
+  _irParaAdocao = () => {
+    this.props.navigation.navigate('Adocao');
+  };
 
-    _irParaAdocao = () => {
-        this.props.navigation.navigate('Adocao');
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={{flex: 2, justifyContent: 'center', marginTop: 20}}>
-                <Text style={styles.title}>iPet</Text>
-                </View>
-                <View style={{flex: 4, justifyContent: 'center'}}>
-                <TouchableHighlight style={styles.button} onPress={() => {this._irParaAdocao()}} >
-                    <Text style={styles.text}>ADOÇÃO</Text>
-                </TouchableHighlight>
-                <TouchableHighlight style={styles.button}>
-                    <Text style={styles.text}>DOAÇÃO</Text>
-                </TouchableHighlight>
-                </View>
-                <View style={styles.viewImage}>
-                <Image source={require('../../imgs/patas.png')} style={styles.image} />
-                </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{flex: 2, justifyContent: 'center', marginTop: 20}}>
+          <Text style={styles.title}>iPet</Text>
+        </View>
+        <View style={{flex: 4, justifyContent: 'center'}}>
+          <ButtonRoutes onPress={() => this._irParaAdocao()} name='ADOÇÃO'/>
+          <ButtonRoutes onPress={() => this._irParaAdocao()} name='DOAÇÃO'/>
+        </View>
+        <View style={styles.viewImage}>
+          <Image source={require('../../imgs/patas.png')} style={styles.image} />
+        </View>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -44,25 +39,10 @@ const styles = StyleSheet.create({
     //justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
   title: {
     color: 'black',
     fontSize: 60,
     fontFamily: 'GROBOLD',
-  },
-  button: {
-    backgroundColor: '#2fb7a7',
-    padding: 10,
-    borderRadius: 5,
-    margin: 10,
-    width: Dimensions.get('window').width / 1.5,
-    height: Dimensions.get('window').height / 10,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   viewImage: {
     flex: 3,
